ff4cae99c42f9de65ecc604472789d0af90e14f7
bibliutbm <bibliotheque.utbm@gmail.com>
2023-05-15 11:18:00 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/to52_international_student_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.to52_international_student_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Theme/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+//Test de thème pour l'application
+
+var firstColor = const Color(0xFFFAB0B3);
+var secondColor = const Color(0xFF00919C);
+var thirdColor = const Color(0xFF002782);
+var fourthColor = const Color(0xFFFFDD02);
+var background = const Color(0xFFF7F6FB);
+
+
+var primaryText = Colors.black54;
